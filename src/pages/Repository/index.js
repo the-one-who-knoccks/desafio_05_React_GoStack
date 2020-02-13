e48f2f7ff41e7bcc6c7c88edb5226279c1cc640a
the-one-who-knoccks <thiagoalves.informatica@gmail.com>
2020-02-13 08:35:55 -0300

    fase03_React_GoStack

---
+++ src/pages/Repository/index.js
@@ -0,0 +1,86 @@
+/* eslint-disable react/static-property-placement */
+/* eslint-disable react/state-in-constructor */
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
+
+import Container from '../../components/container';
+
+import { Loading, Owner, IssueList } from './styles';
+
+export default class Repository extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string // Tipo do valor que vem dentro da url
+      })
+    }).isRequired
+  };
+
+  state = {
+    repository: {},
+    issues: [],
+    loading: true
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    // Executa as duas chamadas api juntas e
+    const [repository, issues] = await Promise.all([
+      // retorna primeira pos em repository e a segunda em issues.
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5
+        }
+      })
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false
+    });
+  }
+
+  render() {
+    // eslint-disable-next-line no-unused-vars
+    const { repository, issues, loading } = this.state;
+
+    if (loading) {
+      return <Loading>Carregando</Loading>;
+    }
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Voltar aos repositórios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+
+        <IssueList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
+      </Container>
+    );
+  }
+}
